2a09a4d4c7cce15d32885360264d384adb1d604e
nagosu <nozick1021@gmail.com>
2023-11-07 18:21:53 +0900

    프로젝트 초기 세팅

---
+++ src/pages/DetailPage.tsx
@@ -0,0 +1,9 @@
+function DetailPage() {
+  return (
+    <div>
+      <h1>상세페이지 입니다. 알아서 수정하십쇼.</h1>
+    </div>
+  );
+}
+
+export default DetailPage;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+function MainPage() {
+  return (
+    <div>
+      <h1>메인페이지 입니다. 알아서 수정하십쇼.</h1>
+    </div>
+  );
+}
+
+export default MainPage;
