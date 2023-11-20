f8faef4e75efbc3073b2ba60e0759e962018bf15
Ajeong-Im <jenny2360@naver.com>
2023-11-20 14:02:14 +0900

    feat : 카테고리 페이지 1차 디자인

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,7 +1,168 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
+import shoes from '../assets/shoes/shoes.png';
+//  import { CategoryType } from '../utils/types';
 
-function CategoryPage() {
-  return <NavBar />;
+const CategoryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  height: 46rem;
+  margin: 24px auto;
+  text-align: left;
+`;
+
+const CategoryTitle = styled.span`
+  font-family: Pretendard;
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-top: 20px;
+  margin-bottom: 30px;
+  text-align: left;
+  width: 90%;
+`;
+
+const CategoryItemContainer = styled.div`
+  display: grid;
+  width: 90%;
+  grid-template-columns: repeat(4, 1fr); // 4개의 열
+  grid-template-rows: repeat(2, 1fr); // 2개의 행
+  gap: 20px; // 셀 간격 조절
+`;
+
+const CategoryContentContainer = styled(Link)`
+  display: flex;
+  width: 100%;
+  height: 22rem;
+  flex-direction: column;
+  align-items: left;
+  text-decoration: none;
+  color: inherit;
+  &:hover {
+    background-color: #ddd;
+  }
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const CategoryImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 18rem;
+  background-color: #e8e8e8;
+`;
+
+const ShoesImage = styled.img`
+  width: 100%;
+`;
+
+const CategoryBrandTitle = styled.span`
+  font-family: Pretendard;
+  font-size: 0.8rem;
+  font-weight: 600;
+  margin-bottom: 5px;
+  margin-top: 3px;
+  margin-left: 5px;
+  color: #959595;
+`;
+
+const CategoryProductTitle = styled.span`
+  font-family: Pretendard;
+  font-size: 0.8rem;
+  font-weight: 600;
+  margin-bottom: 0;
+  margin-top: 3px;
+  margin-left: 5px;
+`;
+
+const CategoryPriceTitle = styled.span`
+  font-family: Pretendard;
+  font-size: 0.8rem;
+  font-weight: 600;
+  margin-bottom: 5px;
+  margin-top: 0;
+  margin-left: 5px;
+  color: #959595;
+`;
+
+export default function CategoryPage() {
+  return (
+    <>
+      <NavBar />
+      <CategoryContainer>
+        <CategoryTitle>스니커즈</CategoryTitle>
+        <CategoryItemContainer>
+          <CategoryContentContainer to="/detail">
+            <CategoryImageContainer>
+              <ShoesImage src={shoes} alt="shoes image" />
+            </CategoryImageContainer>
+            <CategoryBrandTitle>NIKE</CategoryBrandTitle>
+            <CategoryProductTitle>NIKE W DUNK LOW PRM</CategoryProductTitle>
+            <CategoryPriceTitle>₩ 139,000</CategoryPriceTitle>
+          </CategoryContentContainer>
+          <CategoryContentContainer to="/detail">
+            <CategoryImageContainer>
+              <ShoesImage src={shoes} alt="shoes image" />
+            </CategoryImageContainer>
+            <CategoryBrandTitle>NIKE</CategoryBrandTitle>
+            <CategoryProductTitle>NIKE W DUNK LOW PRM</CategoryProductTitle>
+            <CategoryPriceTitle>₩ 139,000</CategoryPriceTitle>
+          </CategoryContentContainer>
+          <CategoryContentContainer to="/detail">
+            <CategoryImageContainer>
+              <ShoesImage src={shoes} alt="shoes image" />
+            </CategoryImageContainer>
+            <CategoryBrandTitle>NIKE</CategoryBrandTitle>
+            <CategoryProductTitle>NIKE W DUNK LOW PRM</CategoryProductTitle>
+            <CategoryPriceTitle>₩ 139,000</CategoryPriceTitle>
+          </CategoryContentContainer>
+          <CategoryContentContainer to="/detail">
+            <CategoryImageContainer>
+              <ShoesImage src={shoes} alt="shoes image" />
+            </CategoryImageContainer>
+            <CategoryBrandTitle>NIKE</CategoryBrandTitle>
+            <CategoryProductTitle>NIKE W DUNK LOW PRM</CategoryProductTitle>
+            <CategoryPriceTitle>₩ 139,000</CategoryPriceTitle>
+          </CategoryContentContainer>
+          <CategoryContentContainer to="/detail">
+            <CategoryImageContainer>
+              <ShoesImage src={shoes} alt="shoes image" />
+            </CategoryImageContainer>
+            <CategoryBrandTitle>NIKE</CategoryBrandTitle>
+            <CategoryProductTitle>NIKE W DUNK LOW PRM</CategoryProductTitle>
+            <CategoryPriceTitle>₩ 139,000</CategoryPriceTitle>
+          </CategoryContentContainer>
+          <CategoryContentContainer to="/detail">
+            <CategoryImageContainer>
+              <ShoesImage src={shoes} alt="shoes image" />
+            </CategoryImageContainer>
+            <CategoryBrandTitle>NIKE</CategoryBrandTitle>
+            <CategoryProductTitle>NIKE W DUNK LOW PRM</CategoryProductTitle>
+            <CategoryPriceTitle>₩ 139,000</CategoryPriceTitle>
+          </CategoryContentContainer>
+          <CategoryContentContainer to="/detail">
+            <CategoryImageContainer>
+              <ShoesImage src={shoes} alt="shoes image" />
+            </CategoryImageContainer>
+            <CategoryBrandTitle>NIKE</CategoryBrandTitle>
+            <CategoryProductTitle>NIKE W DUNK LOW PRM</CategoryProductTitle>
+            <CategoryPriceTitle>₩ 139,000</CategoryPriceTitle>
+          </CategoryContentContainer>
+          <CategoryContentContainer to="/detail">
+            <CategoryImageContainer>
+              <ShoesImage src={shoes} alt="shoes image" />
+            </CategoryImageContainer>
+            <CategoryBrandTitle>NIKE</CategoryBrandTitle>
+            <CategoryProductTitle>NIKE W DUNK LOW PRM</CategoryProductTitle>
+            <CategoryPriceTitle>₩ 139,000</CategoryPriceTitle>
+          </CategoryContentContainer>
+        </CategoryItemContainer>
+      </CategoryContainer>
+    </>
+  );
 }
-
-export default CategoryPage;
---
+++ src/utils/types.ts
@@ -0,0 +1,6 @@
+export type CategoryType = {
+  id: number;
+  image: string;
+  name: string;
+  price: string;
+};
